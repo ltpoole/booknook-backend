4ad429f97ee4ca4c7e1de4dee8c38141ee5ed0d8
ltpoole <laborris.poole@gmail.com>
2023-12-18 21:11:30 -0500

    created Review model

---
+++ FullStackAuth_WebAPI/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Review
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+        public string BookId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set;}
+
+        public User User { get; set;}
+    }
+}
