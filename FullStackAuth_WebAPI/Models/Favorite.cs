87bedb9984138e44f0716c9e5214ebdb05835898
ltpoole <laborris.poole@gmail.com>
2023-12-18 21:16:11 -0500

    created Favorite model

---
+++ FullStackAuth_WebAPI/Models/Favorite.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string ThumbnailUrl { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
