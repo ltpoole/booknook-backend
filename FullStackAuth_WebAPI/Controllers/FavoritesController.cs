52b64e435cb7344c06d0c37b453a831017a11774
ltpoole <laborris.poole@gmail.com>
2023-12-19 00:00:57 -0500

    created FavoritesController with a GET endpoint

---
+++ FullStackAuth_WebAPI/Controllers/FavoritesController.cs
@@ -0,0 +1,36 @@
+﻿using FullStackAuth_WebAPI.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("myFavorites"), Authorize]
+        public IActionResult GetUsersFavorites() 
+        {
+            try
+            {
+                string userID = User.FindFirstValue("id");
+
+                var favorites = _context.Favorites.Where(f => f.UserId.Equals(userID));
+
+                return StatusCode(200, favorites);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
